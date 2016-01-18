3793619405a0e5843bd691ba217a827b10c8e2bf
Abigail Watson <awatson1978@gmail.com>
2016-01-17 16:27:49 -0800

    updated package.js

--- package.js
+++ package.js
@@ -9,7 +9,7 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('1.1.0.3');
 
-  api.use('ground:db@0.3.14');
+  api.use('ground:db@0.3.12');
   api.addFiles('offline.js');
 });
 
